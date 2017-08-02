86a2a4f470f55ab3968ac09f686402b172bac2eb
Andrey Evstropov <eandreyf@yandex-team.ru>
2017-08-02 03:02:31 +0300

    change node listener

--- app/show.js
+++ app/show.js
@@ -1,8 +1,9 @@
+import fs from 'fs';
 import {events} from './events';
 import {normalize} from './normalize';
 
 export default () => {
     const result = JSON.stringify(normalize(events), null, 4);
 
-    fs.writeFile('output.json', result);
+    fs.writeFileSync('./app/output.json', result);
 }
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+import express from 'express';
+import normalizeEvents from './app/show';
+
 const app = express();
 
 app.use(express.static('app'));
 
 app.listen(3000, function () {
+    normalizeEvents();
     console.log('Example app listening on port 3000!');
 });
